43003523ee91c4139116d78b3e7e9f29896abf32
nasimqureshi <qureshi.nasim@yahoo.com>
2021-02-07 13:05:12 -0600

    second commit

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -2,15 +2,21 @@ import React, { useState } from 'react'
 import './App.css';
 import Form from "./components/Form"
 import TodoList from "./components/ToDoList"
+import Todo from './components/Todo'
 
 function App() {
   const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
    <div className="App">
     <header>
     <h1>My Todo List</h1>
     </header>
-    <Form setInputText={setInputText} />
+    <Form 
+    inputText={inputText}
+    todos={todos}
+     setTodos={setTodos} 
+     setInputText={setInputText} />
     <TodoList />
    </div>
   );
